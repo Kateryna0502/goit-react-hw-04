dc3d59e4d9a21e109a7d7b30e7491d1ed1339016
Kateryna <mweoliss74@gmail.com>
2024-08-15 22:34:30 +0300

    d

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,134 @@
+import Modal from 'react-modal';
+import css from './ImageModal.module.css';
+Modal.setAppElement('#root');
+
+const customStyles = {
+  content: {
+    width: '60%',
+    height: '90%',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+};
+const ImageModal = ({ modalIsOpen, closeModal, currentPhoto }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === "Escape") {
+        onCloseModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onCloseModal]);
+  
+    return (
+    <Modal
+      style={customStyles}
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+      shouldCloseOnOverlayClick={true}
+    >
+      <img
+        className={css.photo}
+        src={currentPhoto.url}
+        alt={currentPhoto.alt}
+      />
+    </Modal>
+  );
+};
+
+export default ImageModal;
+
+
+
+
+// import { useEffect, useRef, useState } from "react";
+// import css from "./Modal.module.css";
+// import { createPortal } from "react-dom";
+// import AddProfileForm from "../AddProfileForm/AddProfileForm";
+
+
+// const Modal = ({ onCloseModal, serverData }) => {
+//   const [counter, setCounter] = useState(() => {
+//     return parseInt(localStorage.getItem("counterValue") ?? 0);
+//   });
+//   const buttonRef = useRef(null);
+//   const secondButtonRef = useRef(null);
+
+//   useEffect(() => {
+//     localStorage.setItem("counterValue", counter);
+//   }, [counter]);
+
+//   useEffect(() => {
+//     const handleKeyDown = (event) => {
+//       if (event.code === "Escape") {
+//         onCloseModal();
+//       }
+//     };
+
+//     window.addEventListener("keydown", handleKeyDown);
+
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//     };
+//   }, [onCloseModal]);
+
+//   const handleBackDropClick = (event) => {
+//     if (event.target === event.currentTarget) {
+//       onCloseModal();
+//     }
+//   };
+
+//   const handleTestRef = () => {
+//     secondButtonRef.current = "Hello Alibaba";
+//   };
+
+//   return createPortal(
+//     <div onClick={handleBackDropClick} className={css.backdrop}>
+//       <div className={css.modal}>
+//         <button
+//           type="button"
+//           aria-label="Close modal button"
+//           className={css.closeModalBtn}
+//           onClick={onCloseModal}
+//         >
+//           &times;
+//         </button>
+
+//         <h3 className={css.title}>Modal</h3>
+//         <p className={css.text}>
+//           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet in
+//           doloremque laboriosam tempore reprehenderit maiores nobis eligendi ad
+//           ipsum facere!
+//         </p>
+//         <button ref={buttonRef} onClick={handleTestRef} type="button">
+//           Click to focus on the second button
+//         </button>
+//         <button
+//           ref={secondButtonRef}
+//           type="button"
+//           onClick={() => setCounter(counter + 1)}
+//         >
+//           Click to increment counter: {counter}
+//         </button>
+
+//        <p>Server Data: {serverData}</p>
+//       </div>
+//     </div>,
+//     document.getElementById("modal-root")
+//   );
+// };
+
+// export default Modal;
---
+++ src/services/api.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const requestPhotos = async (query, pageNumber) => {
+    const params = {
+    query: query,
+    page: pageNumber,
+    orientation: "landscape",
+    per_page: 20,
+  };
+  const { data } = await axios.get("https://api.unsplash.com/search/photos?client_id=lD1P932jYFcxUcvysE1bhjA5NVgfVCLU2ML6haokAUE",
+    {
+      params,
+      headers: {
+        "Accept-Version": "v1",
+      },
+    }
+  );
+  return data;
+};
+
+
+
+export default requestPhotos;
